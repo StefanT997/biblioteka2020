799430e77c62c5f8a4295dbd038587b7640c87eb
StefanT997 <fanstetosic97@gmail.com>
2020-02-19 15:10:01 +0100

    Ispravljen interfejs BibliotekaInterfejs

--- Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -1,6 +1,6 @@
 package biblioteka.interfejs;
 
-import java.util.LinkedList;
+import java.util.List;
 
 import biblioteka.Autor;
 import biblioteka.Knjiga;
@@ -10,8 +10,8 @@ public interface BibliotekaInterfejs {
 	
 	public void obrisiKnjigu(Knjiga knjiga);
 	
-	public LinkedList<Knjiga> vratiSveKnjige(); 
+	public List<Knjiga> vratiSveKnjige(); 
 	
-	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
+	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
 	
 }
